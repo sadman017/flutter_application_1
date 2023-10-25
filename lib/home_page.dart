b4e1a4ae5818a17dd24957b3ad656d10bf09b1c4
Sadman017 <sadmanrahman31@gmail.com>
2023-10-25 11:35:29 +0600

    $4.0

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget{
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        actions: [],
+        actions: const [],
         title: const Text("Frenzy Chat"),
       ),
       body: const ChatScreen(),
